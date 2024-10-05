2158779948a77f143fc16b770e53c9983f127e39
Keerthana <168259439+HelloKeerthana@users.noreply.github.com>
2024-10-05 15:52:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function fetchData() {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const imgElement = document.getElementById("pokemonSprite");
+        const loadingElement = document.getElementById("loading");
+      
+        loadingElement.style.display = "block";
+        imgElement.style.display = "none";
+
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+            if (!response.ok) {
+                throw new Error("Pokémon not found");
+            }
+
+            const data = await response.json();
+            const pokemonSprite = data.sprites.front_default;
+
+            imgElement.src = pokemonSprite;
+            imgElement.style.display = "block";
+        } catch (error) {
+            console.error(error);
+            alert("Failed to fetch Pokémon. Please try again.");
+        } finally {
+           loadingElement.style.display = "none";
+        }
+
+        document.getElementById("pokemonName").value = '';
+    }
